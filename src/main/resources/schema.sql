e642eba5f7f59f2f3b0a8a49042ea3bf446741af
Alex <klaussr@mail.ru>
2021-03-23 18:54:46 +1000

    test

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS organizations (
 );
 COMMENT ON TABLE Person IS 'Человек';
 
-CREATE TABLE IF NOT EXISTS House (
+CREATE TABLE IF NOT EXISTS offices (
     id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
     version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
     address    VARCHAR(50) NOT NULL COMMENT 'Адрес'
